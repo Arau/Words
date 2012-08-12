572a7def7811aaa2a45477a6a0b319ac3f41bcaa
Ferran Arau <ferran.arau.castell@gmail.com>
2012-08-12 20:59:02 +0200

    Finish game layout

--- src/com/idi/arau/finishGame.java
+++ src/com/idi/arau/finishGame.java
@@ -2,15 +2,34 @@ package com.idi.arau;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class finishGame extends Activity {
+
+	Button button;
+
 	@Override
-	public void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);		
-		setContentView(R.layout.finish);		
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.finish);
 		Toast toast = Toast.makeText(this, "Finished game", Toast.LENGTH_SHORT);
-		toast.show();		
+		toast.show();
+		button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				toast();
+			}
+		});
 	}
+
+	private void toast() {
+		Toast t = Toast.makeText(this, "press", Toast.LENGTH_SHORT);
+		t.show();
+	}
+
 }
